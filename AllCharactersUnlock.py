72890c6e597667b051df3d589e3e672ec3f1be92
MEGA-MIND <53368071+MEGA-MIND@users.noreply.github.com>
2019-09-01 10:50:10 +0530

    New all characters unlock
    Njoy

---
+++ AllCharactersUnlock.py
@@ -0,0 +1,523 @@
+import bsSpaz as newChar
+
+""" Coded by RK
+    You can change name of characters
+    that are marked with underscore '_'
+    but others are reserved characters
+    so if you want to sustain their 
+    powers on main moded platform then
+    keep their name as it is..
+
+"""
+
+t = newChar.Appearance("_Zoe_")
+
+t.colorTexture = "zoeColor"
+t.colorMaskTexture = "zoeColorMask"
+
+t.defaultColor = (0.6, 0.6, 0.6)
+t.defaultHighlight = (0, 1, 0)
+
+t.iconTexture = "zoeIcon"
+t.iconMaskTexture = "zoeIconColorMask"
+
+t.headModel = "zoeHead"
+t.torsoModel = "zoeTorso"
+t.pelvisModel = "zoePelvis"
+t.upperArmModel = "zoeUpperArm"
+t.foreArmModel = "zoeForeArm"
+t.handModel = "zoeHand"
+t.upperLegModel = "zoeUpperLeg"
+t.lowerLegModel = "zoeLowerLeg"
+t.toesModel = "zoeToes"
+t.jumpSounds=["zoeJump01",
+              "zoeJump02",
+              "zoeJump03"]
+t.attackSounds=["zoeAttack01",
+                "zoeAttack02",
+                "zoeAttack03",
+                "zoeAttack04"]
+t.impactSounds=["zoeImpact01",
+                "zoeImpact02",
+                "zoeImpact03",
+                "zoeImpact04"]
+t.deathSounds=["zoeDeath01"]
+t.pickupSounds=["zoePickup01"]
+t.fallSounds=["zoeFall01"]
+
+t.style = 'female'
+
+
+###############  Ninja   ##################
+t = newChar.Appearance("_Snake Shadow_")
+
+t.colorTexture = "ninjaColor"
+t.colorMaskTexture = "ninjaColorMask"
+
+t.defaultColor = (1, 1, 1)
+t.defaultHighlight = (0.55, 0.8, 0.55)
+
+t.iconTexture = "ninjaIcon"
+t.iconMaskTexture = "ninjaIconColorMask"
+
+t.headModel = "ninjaHead"
+t.torsoModel = "ninjaTorso"
+t.pelvisModel = "ninjaPelvis"
+t.upperArmModel = "ninjaUpperArm"
+t.foreArmModel = "ninjaForeArm"
+t.handModel = "ninjaHand"
+t.upperLegModel = "ninjaUpperLeg"
+t.lowerLegModel = "ninjaLowerLeg"
+t.toesModel = "ninjaToes"
+
+ninjaAttacks = ['ninjaAttack'+str(i+1)+'' for i in range(7)]
+ninjaHits = ['ninjaHit'+str(i+1)+'' for i in range(8)]
+ninjaJumps = ['ninjaAttack'+str(i+1)+'' for i in range(7)]
+
+t.jumpSounds=ninjaJumps
+t.attackSounds=ninjaAttacks
+t.impactSounds=ninjaHits
+t.deathSounds=["ninjaDeath1"]
+t.pickupSounds=ninjaAttacks
+t.fallSounds=["ninjaFall1"]
+
+t.style = 'ninja'
+
+
+###############  Kronk   ##################
+t = newChar.Appearance("HULK")
+
+t.colorTexture = "kronk"
+t.colorMaskTexture = "kronkColorMask"
+
+t.defaultColor = (0.4, 0.5, 0.4)
+t.defaultHighlight = (1, 0.5, 0.3)
+
+t.iconTexture = "kronkIcon"
+t.iconMaskTexture = "kronkIconColorMask"
+
+t.headModel = "kronkHead"
+t.torsoModel = "kronkTorso"
+t.pelvisModel = "kronkPelvis"
+t.upperArmModel = "kronkUpperArm"
+t.foreArmModel = "kronkForeArm"
+t.handModel = "kronkHand"
+t.upperLegModel = "kronkUpperLeg"
+t.lowerLegModel = "kronkLowerLeg"
+t.toesModel = "kronkToes"
+
+kronkSounds = ["kronk1",
+              "kronk2",
+              "kronk3",
+              "kronk4",
+              "kronk5",
+              "kronk6",
+              "kronk7",
+              "kronk8",
+              "kronk9",
+              "kronk10"]
+t.jumpSounds=kronkSounds
+t.attackSounds=kronkSounds
+t.impactSounds=kronkSounds
+t.deathSounds=["kronkDeath"]
+t.pickupSounds=kronkSounds
+t.fallSounds=["kronkFall"]
+
+t.style = 'kronk'
+
+
+###############  MEL   ##################
+t = newChar.Appearance("_Mel_")
+
+t.colorTexture = "melColor"
+t.colorMaskTexture = "melColorMask"
+
+t.defaultColor = (1, 1, 1)
+t.defaultHighlight = (0.1, 0.6, 0.1)
+
+t.iconTexture = "melIcon"
+t.iconMaskTexture = "melIconColorMask"
+
+t.headModel =     "melHead"
+t.torsoModel =    "melTorso"
+t.pelvisModel = "kronkPelvis"
+t.upperArmModel = "melUpperArm"
+t.foreArmModel =  "melForeArm"
+t.handModel =     "melHand"
+t.upperLegModel = "melUpperLeg"
+t.lowerLegModel = "melLowerLeg"
+t.toesModel =     "melToes"
+
+melSounds = ["mel01",
+              "mel02",
+              "mel03",
+              "mel04",
+              "mel05",
+              "mel06",
+              "mel07",
+              "mel08",
+              "mel09",
+              "mel10"]
+
+t.attackSounds = melSounds
+t.jumpSounds = melSounds
+t.impactSounds = melSounds
+t.deathSounds=["melDeath01"]
+t.pickupSounds = melSounds
+t.fallSounds=["melFall01"]
+
+t.style = 'mel'
+
+
+###############  Jack Morgan   ##################
+
+t = newChar.Appearance("_Jack Morgan_")
+
+t.colorTexture = "jackColor"
+t.colorMaskTexture = "jackColorMask"
+
+t.defaultColor = (1, 0.2, 0.1)
+t.defaultHighlight = (1, 1, 0)
+
+t.iconTexture = "jackIcon"
+t.iconMaskTexture = "jackIconColorMask"
+
+t.headModel =     "jackHead"
+t.torsoModel =    "jackTorso"
+t.pelvisModel = "kronkPelvis"
+t.upperArmModel = "jackUpperArm"
+t.foreArmModel =  "jackForeArm"
+t.handModel =     "jackHand"
+t.upperLegModel = "jackUpperLeg"
+t.lowerLegModel = "jackLowerLeg"
+t.toesModel =     "jackToes"
+
+hitSounds = ["jackHit01",
+             "jackHit02",
+             "jackHit03",
+             "jackHit04",
+             "jackHit05",
+             "jackHit06",
+             "jackHit07"]
+
+sounds = ["jack01",
+          "jack02",
+          "jack03",
+          "jack04",
+          "jack05",
+          "jack06"]
+
+t.attackSounds = sounds
+t.jumpSounds = sounds
+t.impactSounds = hitSounds
+t.deathSounds=["jackDeath01"]
+t.pickupSounds = sounds
+t.fallSounds=["jackFall01"]
+
+t.style = 'pirate'
+
+
+###############  SANTA   ##################
+
+t = newChar.Appearance("_Santa Claus_")
+
+t.colorTexture = "santaColor"
+t.colorMaskTexture = "santaColorMask"
+
+t.defaultColor = (1, 0, 0)
+t.defaultHighlight = (1, 1, 1)
+
+t.iconTexture = "santaIcon"
+t.iconMaskTexture = "santaIconColorMask"
+
+t.headModel =     "santaHead"
+t.torsoModel =    "santaTorso"
+t.pelvisModel = "kronkPelvis"
+t.upperArmModel = "santaUpperArm"
+t.foreArmModel =  "santaForeArm"
+t.handModel =     "santaHand"
+t.upperLegModel = "santaUpperLeg"
+t.lowerLegModel = "santaLowerLeg"
+t.toesModel =     "santaToes"
+
+hitSounds = ['santaHit01', 'santaHit02', 'santaHit03', 'santaHit04']
+sounds = ['santa01', 'santa02', 'santa03', 'santa04', 'santa05']
+
+t.attackSounds = sounds
+t.jumpSounds = sounds
+t.impactSounds = hitSounds
+t.deathSounds=["santaDeath"]
+t.pickupSounds = sounds
+t.fallSounds=["santaFall"]
+
+t.style = 'santa'
+
+###############  FROSTY   ##################
+
+t = newChar.Appearance("_Frosty_")
+
+t.colorTexture = "frostyColor"
+t.colorMaskTexture = "frostyColorMask"
+
+t.defaultColor = (0.5, 0.5, 1)
+t.defaultHighlight = (1, 0.5, 0)
+
+t.iconTexture = "frostyIcon"
+t.iconMaskTexture = "frostyIconColorMask"
+
+t.headModel =     "frostyHead"
+t.torsoModel =    "frostyTorso"
+t.pelvisModel = "frostyPelvis"
+t.upperArmModel = "frostyUpperArm"
+t.foreArmModel =  "frostyForeArm"
+t.handModel =     "frostyHand"
+t.upperLegModel = "frostyUpperLeg"
+t.lowerLegModel = "frostyLowerLeg"
+t.toesModel =     "frostyToes"
+
+frostySounds = ['frosty01', 'frosty02', 'frosty03', 'frosty04', 'frosty05']
+frostyHitSounds = ['frostyHit01', 'frostyHit02', 'frostyHit03']
+
+t.attackSounds = frostySounds
+t.jumpSounds = frostySounds
+t.impactSounds = frostyHitSounds
+t.deathSounds=["frostyDeath"]
+t.pickupSounds = frostySounds
+t.fallSounds=["frostyFall"]
+
+t.style = 'frosty'
+
+###############  BONES  ##################
+
+t = newChar.Appearance("DEATH")
+
+t.colorTexture = "bonesColor"
+t.colorMaskTexture = "bonesColorMask"
+
+t.defaultColor = (0.6, 0.9, 1)
+t.defaultHighlight = (0.6, 0.9, 1)
+
+t.iconTexture = "bonesIcon"
+t.iconMaskTexture = "bonesIconColorMask"
+
+t.headModel =     "bonesHead"
+t.torsoModel =    "bonesTorso"
+t.pelvisModel =   "bonesPelvis"
+t.upperArmModel = "bonesUpperArm"
+t.foreArmModel =  "bonesForeArm"
+t.handModel =     "bonesHand"
+t.upperLegModel = "bonesUpperLeg"
+t.lowerLegModel = "bonesLowerLeg"
+t.toesModel =     "bonesToes"
+
+bonesSounds =    ['bones1', 'bones2', 'bones3']
+bonesHitSounds = ['bones1', 'bones2', 'bones3']
+
+t.attackSounds = bonesSounds
+t.jumpSounds = bonesSounds
+t.impactSounds = bonesHitSounds
+t.deathSounds=["bonesDeath"]
+t.pickupSounds = bonesSounds
+t.fallSounds=["bonesFall"]
+
+t.style = 'bones'
+
+# bear ###################################
+
+t = newChar.Appearance("_Bernard_")
+
+t.colorTexture = "bearColor"
+t.colorMaskTexture = "bearColorMask"
+
+t.defaultColor = (0.7, 0.5, 0.0)
+#t.defaultHighlight = (0.6, 0.5, 0.8)
+
+t.iconTexture = "bearIcon"
+t.iconMaskTexture = "bearIconColorMask"
+
+t.headModel =     "bearHead"
+t.torsoModel =    "bearTorso"
+t.pelvisModel =   "bearPelvis"
+t.upperArmModel = "bearUpperArm"
+t.foreArmModel =  "bearForeArm"
+t.handModel =     "bearHand"
+t.upperLegModel = "bearUpperLeg"
+t.lowerLegModel = "bearLowerLeg"
+t.toesModel =     "bearToes"
+
+bearSounds =    ['bear1', 'bear2', 'bear3', 'bear4']
+bearHitSounds = ['bearHit1', 'bearHit2']
+
+t.attackSounds = bearSounds
+t.jumpSounds = bearSounds
+t.impactSounds = bearHitSounds
+t.deathSounds=["bearDeath"]
+t.pickupSounds = bearSounds
+t.fallSounds=["bearFall"]
+
+t.style = 'bear'
+
+# Penguin ###################################
+
+t = newChar.Appearance("_Pascal_")
+
+t.colorTexture = "penguinColor"
+t.colorMaskTexture = "penguinColorMask"
+
+t.defaultColor = (0.3, 0.5, 0.8)
+t.defaultHighlight = (1, 0, 0)
+
+t.iconTexture = "penguinIcon"
+t.iconMaskTexture = "penguinIconColorMask"
+
+t.headModel =     "penguinHead"
+t.torsoModel =    "penguinTorso"
+t.pelvisModel =   "penguinPelvis"
+t.upperArmModel = "penguinUpperArm"
+t.foreArmModel =  "penguinForeArm"
+t.handModel =     "penguinHand"
+t.upperLegModel = "penguinUpperLeg"
+t.lowerLegModel = "penguinLowerLeg"
+t.toesModel =     "penguinToes"
+
+penguinSounds =    ['penguin1', 'penguin2', 'penguin3', 'penguin4']
+penguinHitSounds = ['penguinHit1', 'penguinHit2']
+
+t.attackSounds = penguinSounds
+t.jumpSounds = penguinSounds
+t.impactSounds = penguinHitSounds
+t.deathSounds=["penguinDeath"]
+t.pickupSounds = penguinSounds
+t.fallSounds=["penguinFall"]
+
+t.style = 'penguin'
+
+
+# Ali ###################################
+t = newChar.Appearance("DUST")
+t.colorTexture = "aliColor"
+t.colorMaskTexture = "aliColorMask"
+t.defaultColor = (1, 0.5, 0)
+t.defaultHighlight = (1, 1, 1)
+t.iconTexture = "aliIcon"
+t.iconMaskTexture = "aliIconColorMask"
+t.headModel =     "aliHead"
+t.torsoModel =    "aliTorso"
+t.pelvisModel =   "aliPelvis"
+t.upperArmModel = "aliUpperArm"
+t.foreArmModel =  "aliForeArm"
+t.handModel =     "aliHand"
+t.upperLegModel = "aliUpperLeg"
+t.lowerLegModel = "aliLowerLeg"
+t.toesModel =     "aliToes"
+aliSounds =    ['ali1', 'ali2', 'ali3', 'ali4']
+aliHitSounds = ['aliHit1', 'aliHit2']
+t.attackSounds = aliSounds
+t.jumpSounds = aliSounds
+t.impactSounds = aliHitSounds
+t.deathSounds=["aliDeath"]
+t.pickupSounds = aliSounds
+t.fallSounds=["aliFall"]
+t.style = 'ali'
+
+# cyborg ###################################
+t = newChar.Appearance("FLASH")
+t.colorTexture = "cyborgColor"
+t.colorMaskTexture = "cyborgColorMask"
+t.defaultColor = (0.5, 0.5, 0.5)
+t.defaultHighlight = (1, 0, 0)
+t.iconTexture = "cyborgIcon"
+t.iconMaskTexture = "cyborgIconColorMask"
+t.headModel =     "cyborgHead"
+t.torsoModel =    "cyborgTorso"
+t.pelvisModel =   "cyborgPelvis"
+t.upperArmModel = "cyborgUpperArm"
+t.foreArmModel =  "cyborgForeArm"
+t.handModel =     "cyborgHand"
+t.upperLegModel = "cyborgUpperLeg"
+t.lowerLegModel = "cyborgLowerLeg"
+t.toesModel =     "cyborgToes"
+cyborgSounds =    ['cyborg1', 'cyborg2', 'cyborg3', 'cyborg4']
+cyborgHitSounds = ['cyborgHit1', 'cyborgHit2']
+t.attackSounds = cyborgSounds
+t.jumpSounds = cyborgSounds
+t.impactSounds = cyborgHitSounds
+t.deathSounds=["cyborgDeath"]
+t.pickupSounds = cyborgSounds
+t.fallSounds=["cyborgFall"]
+t.style = 'cyborg'
+t = newChar.Appearance("FROZEN KING")
+t.colorTexture = "agentColor"
+t.colorMaskTexture = "agentColorMask"
+t.defaultColor = (0.3, 0.3, 0.33)
+t.defaultHighlight = (1, 0.5, 0.3)
+t.iconTexture = "agentIcon"
+t.iconMaskTexture = "agentIconColorMask"
+t.headModel =     "agentHead"
+t.torsoModel =    "agentTorso"
+t.pelvisModel =   "agentPelvis"
+t.upperArmModel = "agentUpperArm"
+t.foreArmModel =  "agentForeArm"
+t.handModel =     "agentHand"
+t.upperLegModel = "agentUpperLeg"
+t.lowerLegModel = "agentLowerLeg"
+t.toesModel =     "agentToes"
+agentSounds =    ['agent1', 'agent2', 'agent3', 'agent4']
+agentHitSounds = ['agentHit1', 'agentHit2']
+t.attackSounds = agentSounds
+t.jumpSounds = agentSounds
+t.impactSounds = agentHitSounds
+t.deathSounds=["agentDeath"]
+t.pickupSounds = agentSounds
+t.fallSounds=["agentFall"]
+t.style = 'agent'
+t = newChar.Appearance("BOOM")
+t.colorTexture = "pixieColor"
+t.colorMaskTexture = "pixieColorMask"
+t.defaultColor = (0, 1, 0.7)
+t.defaultHighlight = (0.65, 0.35, 0.75)
+t.iconTexture = "pixieIcon"
+t.iconMaskTexture = "pixieIconColorMask"
+t.headModel =     "pixieHead"
+t.torsoModel =    "pixieTorso"
+t.pelvisModel =   "pixiePelvis"
+t.upperArmModel = "pixieUpperArm"
+t.foreArmModel =  "pixieForeArm"
+t.handModel =     "pixieHand"
+t.upperLegModel = "pixieUpperLeg"
+t.lowerLegModel = "pixieLowerLeg"
+t.toesModel =     "pixieToes"
+pixieSounds =    ['pixie1', 'pixie2', 'pixie3', 'pixie4']
+pixieHitSounds = ['pixieHit1', 'pixieHit2']
+t.attackSounds = pixieSounds
+t.jumpSounds = pixieSounds
+t.impactSounds = pixieHitSounds
+t.deathSounds=["pixieDeath"]
+t.pickupSounds = pixieSounds
+t.fallSounds=["pixieFall"]
+t.style = 'pixie'
+t = newChar.Appearance("_Easter Bunny_")
+t.colorTexture = "bunnyColor"
+t.colorMaskTexture = "bunnyColorMask"
+t.defaultColor = (1, 1, 1)
+t.defaultHighlight = (1, 0.5, 0.5)
+t.iconTexture = "bunnyIcon"
+t.iconMaskTexture = "bunnyIconColorMask"
+t.headModel =     "bunnyHead"
+t.torsoModel =    "bunnyTorso"
+t.pelvisModel =   "bunnyPelvis"
+t.upperArmModel = "bunnyUpperArm"
+t.foreArmModel =  "bunnyForeArm"
+t.handModel =     "bunnyHand"
+t.upperLegModel = "bunnyUpperLeg"
+t.lowerLegModel = "bunnyLowerLeg"
+t.toesModel =     "bunnyToes"
+bunnySounds =    ['bunny1', 'bunny2', 'bunny3', 'bunny4']
+bunnyHitSounds = ['bunnyHit1', 'bunnyHit2']
+t.attackSounds = bunnySounds
+t.jumpSounds = ['bunnyJump']
+t.impactSounds = bunnyHitSounds
+t.deathSounds=["bunnyDeath"]
+t.pickupSounds = bunnySounds
+t.fallSounds=["bunnyFall"]
+t.style = 'bunny'
